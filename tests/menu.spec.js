e786dae55e3276999efd8849f403a43bbf921fd8
MPitarma <miguel.pitarma@mindera.com>
2025-10-27 21:59:25 +0000

    Added the test - Menu FORM validation

--- tests/menu.spec.js
+++ tests/menu.spec.js
@@ -12,3 +12,15 @@ test('Menu LOGIN validation', async ({ page }) => {
   await expect(page.getByRole('heading', {name:'LOGIN'})).toBeVisible();
   await expect(page.getByRole('heading', {name:'LOGIN'})).toHaveText(/Login/);
 });
+
+test('Menu FORM validation', async ({ page }) => {
+    await page.goto('https://playground-drab-six.vercel.app/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playground/);
+  
+    await page.getByRole('link', {name:'FORM'}).click();
+  
+    await expect(page.getByRole('heading', {name:'FORM'})).toBeVisible();
+    await expect(page.getByRole('heading', {name:'FORM'})).toHaveText(/Form/);
+  });
